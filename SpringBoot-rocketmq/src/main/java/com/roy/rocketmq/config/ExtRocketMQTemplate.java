51d8dbc93c0617b8453dd152ad07ca93a447cf6a
mendd <menxu_work@163.com>
2022-06-15 13:51:30 +0800

    init

---
+++ SpringBoot-rocketmq/src/main/java/com/roy/rocketmq/config/ExtRocketMQTemplate.java
@@ -0,0 +1,12 @@
+package com.roy.rocketmq.config;
+
+import org.apache.rocketmq.spring.annotation.ExtRocketMQTemplateConfiguration;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+
+/**
+ * @author ：楼兰
+ * @description:
+ **/
+@ExtRocketMQTemplateConfiguration()
+public class ExtRocketMQTemplate extends RocketMQTemplate {
+}
---
+++ rocketMQ-API/src/main/java/org/apache/rocketmq/example/Contact.java
@@ -0,0 +1,6 @@
+package org.apache.rocketmq.example;
+
+public class Contact {
+
+    public static String NAMESRV_ADDR = "rocketmq-2.localhost.com:9876";
+}
